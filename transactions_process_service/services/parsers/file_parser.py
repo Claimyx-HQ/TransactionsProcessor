1d4c618f7739806448ad44acdbe3f10fdf0d8bde
Sharon Fabin <i.sharon.fabin@gmail.com>
2024-04-20 21:33:22 +0300

    Multi bank parsing by name

--- transactions_process_service/services/parsers/file_parser.py
+++ transactions_process_service/services/parsers/file_parser.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Dict, List, Union
+from typing import BinaryIO, Dict, List, Union
 
 from starlette.datastructures import UploadFile
 
@@ -8,5 +8,5 @@ from transactions_process_service.schemas.transaction import Transaction
 
 class FileParser(ABC):
     @abstractmethod
-    def parse_transactions(self, file: UploadFile) -> List[Transaction]:
+    def parse_transactions(self, file: BinaryIO) -> List[Transaction]:
         raise NotImplementedError("parse_transactions method is not implemented")
