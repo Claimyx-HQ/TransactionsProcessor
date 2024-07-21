855e93b6ad5dbb429668dc5deb0297f7a713d8bb
Sharon Fabin <i.sharon.fabin@gmail.com>
2024-07-21 20:37:27 +0300

    added pcc excel parser

---
+++ transactions_processor/services/parsers/system_parsers/pcc/pcc_parser.py
@@ -0,0 +1,37 @@
+import logging
+import math
+from typing import BinaryIO, Dict, List, Union
+import tabula
+import pandas as pd
+import numpy as np
+import re
+from transactions_processor.models.transaction import Transaction
+from datetime import datetime
+
+from transactions_processor.services.parsers.system_parsers.pcc.pcc_csv_parser import (
+    PCCCSVParser,
+)
+from transactions_processor.services.parsers.system_parsers.pcc.pcc_pdf_parser import (
+    PCCPDFParser,
+)
+from transactions_processor.services.parsers.transactions_parser import (
+    TransactionsParser,
+)
+from transactions_processor.utils.math_utils import parse_amount
+
+
+class PCCParser(TransactionsParser):
+    def __init__(self) -> None:
+        self.pdf_parser = PCCPDFParser
+        self.csv_parser = PCCCSVParser
+        self.decoded_data = []
+        self.formated_data = []
+        self.logger = logging.getLogger(__name__)
+
+    def parse_transactions(
+        self, file: BinaryIO, file_key: str | None = None
+    ) -> List[Transaction]:
+        if file.name.endswith(".pdf"):
+            return self.pdf_parser().parse_transactions(file, file_key)
+        else:
+            return self.csv_parser().parse_transactions(file, file_key)
