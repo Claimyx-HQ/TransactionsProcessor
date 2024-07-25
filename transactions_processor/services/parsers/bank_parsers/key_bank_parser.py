0124f62820c5cc2024e190c968c1ac9c727dee00
Ido Brosh <idobroshm@gmail.com>
2024-07-25 13:33:18 +0300

    Added Key Bank

---
+++ transactions_processor/services/parsers/bank_parsers/key_bank_parser.py
@@ -0,0 +1,36 @@
+import logging
+import math
+from typing import Any, BinaryIO, Callable, Dict, List, Union
+import tabula
+import pandas as pd
+import numpy as np
+from transactions_processor.models.transaction import Transaction
+from datetime import datetime
+
+from transactions_processor.services.parsers.pdf_parser import PDFParser
+from transactions_processor.services.parsers.transactions_parser import TransactionsParser
+from transactions_processor.utils.date_utils import valid_date, valid_date_split
+from transactions_processor.utils.math_utils import parse_amount, valid_amount
+
+
+class KeyBankParser(PDFParser):
+    def __init__(self) -> None:
+        super().__init__([130, 166, 241, 510, 580])
+        self.valid_table = True
+
+    def _parse_row(self, row: List[Any], table_index: int) -> Transaction | None:
+        valid_row = valid_date(row[1], "%m-%d")
+        if valid_row and self.valid_table:
+            title_str = row[0]
+            date_str = row[1]
+            amount_str = row[4]
+            description_str = row[3]
+            if title_str == 'Withdrawals':
+                self.valid_table = False
+                return None
+            amount = parse_amount(amount_str)
+            if not valid_amount(amount):
+                return None
+            transaction = Transaction.from_raw_data([date_str, description_str, amount])
+            return transaction
+        return None
