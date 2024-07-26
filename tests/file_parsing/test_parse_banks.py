9a00c235df9e4fb01338839e2e0d0ae0fc9554e3
Ido Brosh <idobroshm@gmail.com>
2024-07-26 13:02:57 +0300

    Added State Bank

--- tests/file_parsing/test_parse_banks.py
+++ tests/file_parsing/test_parse_banks.py
@@ -46,6 +46,7 @@ from transactions_processor.services.parsers.bank_parsers.popular_bank_parser im
 from transactions_processor.services.parsers.bank_parsers.servis1st_bank_parser import (
     Servis1stBankParser,
 )
+from transactions_processor.services.parsers.bank_parsers.state_bank_parser import StateBankParser
 from transactions_processor.services.parsers.bank_parsers.united_bank_parser import (
     UnitedBankParser,
 )
@@ -407,6 +408,22 @@ def test_parse_citizens_bank():
         uuid="2ef3f67e-583e-40f4-bb19-f76dcbaff0c7",
     )
     parser = CitizensBankParser()
+    transactions = parser.parse_transactions(file)
+    parsed_transaction = transactions[0]
+    _check_the_test(first_transaction, parsed_transaction)
+    file.close()
+
+def test_parse_state_bank():
+    logger = logging.getLogger(__name__)
+    file_path = "tests/data/banks/state/state_bank.pdf"
+    file = open(file_path, "rb")
+    first_transaction = Transaction(
+        date=datetime(2024, 6, 3, 0, 0),
+        description="CLARKSVILLE NURS SETTLEMENT",
+        amount=1160.5,
+        uuid="ce5d4279-318c-4dbf-8b5b-b48996e02208",
+    )
+    parser = StateBankParser()
     transactions = parser.parse_transactions(file)
     parsed_transaction = transactions[0]
     _check_the_test(first_transaction, parsed_transaction)
