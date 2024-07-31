942c6e7e5934264ca6f5a77e720549f4b8891468
Ido Brosh <idobroshm@gmail.com>
2024-07-31 21:12:35 +0300

    Refactored Cadence Bank

--- transactions_processor/services/parsers/bank_parsers/cadence_bank_parser.py
+++ transactions_processor/services/parsers/bank_parsers/cadence_bank_parser.py
@@ -1,41 +1,35 @@
-import logging
-import math
-from typing import Any, BinaryIO, Callable, Dict, List, Union
-import tabula
-import pandas as pd
-import numpy as np
+from typing import Any, List, Optional
 from transactions_processor.models.transaction import Transaction
-from datetime import datetime
-
 from transactions_processor.services.parsers.pdf_parser import PDFParser
-from transactions_processor.services.parsers.transactions_parser import TransactionsParser
-from transactions_processor.utils.date_utils import valid_date, valid_date_split
+from transactions_processor.utils.date_utils import valid_date
 from transactions_processor.utils.math_utils import parse_amount, valid_amount
 
 
 class CadenceBankParser(PDFParser):
     def __init__(self) -> None:
-        super().__init__([116,202,484])
+        super().__init__([116, 202, 484])
         self.valid_table = False
 
-    def _parse_row(self, row: List[Any], table_index: int) -> Transaction | None:
-        title = str(str(row[0]) + str(row[1]) ).lower()
-        for title_type in ['DEBITS','DAILY', 'BALANCE', 'SUMMARY']:
-            if title_type.lower() in title:
-                self.valid_table = False
-        for title_type in ['CREDIT','DEPOSIT','ADDITIONS']:
-            if title_type.lower() in title:
-                self.valid_table = True
+    def _parse_row(self, row: List[Any], table_index: int) -> Optional[Transaction]:
+        date_str = str(row[0])
+        amount_str = str(row[1])
+        description_str = str(row[2])
         
-        date_str = row[0] 
-        valid_row = valid_date(date_str, "%m/%d")
-        amount_str = row[1] 
-        description_str = row[2] 
-        if valid_row and self.valid_table and amount_str:
-            amount = parse_amount(amount_str) 
+        title = ''.join([date_str, amount_str]).lower()
+        if any(word in title for word in ['debits', 'daily', 'balance', 'summary']):
+            self.valid_table = False
+        elif any(word in title for word in ['credit', 'deposit', 'additions']):
+            self.valid_table = True
+        
+        if valid_date(date_str, "%m/%d") and self.valid_table and amount_str:
+            try:
+                amount = parse_amount(amount_str)
+            except ValueError:
+                return None
+            
             if not valid_amount(amount):
                 return None
-            transaction = Transaction.from_raw_data([date_str, description_str, amount])
-            return transaction
-   
+
+            return Transaction.from_raw_data([date_str, description_str, amount])
+
         return None
