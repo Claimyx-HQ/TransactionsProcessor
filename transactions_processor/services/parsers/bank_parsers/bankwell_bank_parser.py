561eca1fb1df42fd24d1732f12b99a4138d03799
Ido Brosh <idobroshm@gmail.com>
2024-07-31 21:03:06 +0300

    Refactored Bankwell Bank

--- transactions_processor/services/parsers/bank_parsers/bankwell_bank_parser.py
+++ transactions_processor/services/parsers/bank_parsers/bankwell_bank_parser.py
@@ -1,38 +1,36 @@
-import logging
-import math
-from typing import Any, BinaryIO, Callable, Dict, List, Union
-import tabula
-import pandas as pd
-import numpy as np
+from typing import Any, List, Optional
 from transactions_processor.models.transaction import Transaction
-from datetime import datetime
-
 from transactions_processor.services.parsers.pdf_parser import PDFParser
-from transactions_processor.services.parsers.transactions_parser import TransactionsParser
-from transactions_processor.utils.date_utils import valid_date, valid_date_split
+from transactions_processor.utils.date_utils import valid_date
 from transactions_processor.utils.math_utils import parse_amount, valid_amount
 
 
 class BankWellBankParser(PDFParser):
     def __init__(self) -> None:
-        super().__init__([87,320,410, 485, 570])
+        super().__init__([87, 320, 410, 485, 570])
 
-    def _parse_row(self, row: List[Any], table_index: int) -> Transaction | None:
+    def _parse_row(self, row: List[Any], table_index: int) -> Optional[Transaction]:
         date_str = str(row[0]).replace(' ', '')
-        valid_row = valid_date(date_str, "%m/%d/%Y")
-        description_str = row[1]
+        if not valid_date(date_str, "%m/%d/%Y"):
+            return None
+        
+        description_str = str(row[1])
         amount_str = str(row[3])
         subtraction_amount = str(row[2])
+
         if subtraction_amount != "nan":
             try:
                 subtraction_amount = parse_amount(subtraction_amount)
-            except:
-                pass
-        if valid_row and amount_str != "nan" and subtraction_amount == "nan" :
-            amount = parse_amount(amount_str)
+            except ValueError:
+                return None
+
+        if amount_str != "nan" and (subtraction_amount == 0 or subtraction_amount == "nan"):
+            try:
+                amount = parse_amount(amount_str)
+            except ValueError:
+                return None
             if not valid_amount(amount):
                 return None
-            transaction = Transaction.from_raw_data([date_str, description_str, amount])
-            return transaction
+            return Transaction.from_raw_data([date_str, description_str, amount])
+
         return None
-   
