23a8136554428268e3c960e4be157f2d37d43c67
Sharon Fabin <i.sharon.fabin@gmail.com>
2025-02-19 19:00:36 +0200

    feat: santizie excel table names so they will not fail

--- transactions_processor/services/excel/excel_sorting.py
+++ transactions_processor/services/excel/excel_sorting.py
@@ -1,6 +1,7 @@
 import logging
 from openpyxl.utils import get_column_letter
 from openpyxl.worksheet.table import Table, TableStyleInfo
+import re
 
 
 class ExcelSorting:
@@ -9,12 +10,13 @@ class ExcelSorting:
 
     @staticmethod
     def create_table(worksheet, table_name, matches, row_index, start_col_index):
+        sanitized_name = re.sub(r"[^a-zA-Z0-9_]", "_", table_name)
         # row_index is the first data row
         start_row = row_index - 1  # Header row
         end_row = start_row + len(matches)  # Includes header and data rows
 
         table_range = f"{get_column_letter(start_col_index)}{start_row}:{get_column_letter(start_col_index + 5)}{end_row}"
-        table = Table(displayName=table_name, ref=table_range)
+        table = Table(displayName=sanitized_name, ref=table_range)
         table_style = TableStyleInfo(
             name="TableStyleMedium9",
             showFirstColumn=False,
